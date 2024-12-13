158a03b5de82b92b3aedaf0cecc307bc044b8ba3
wgk133 <wgk00133@gmail.com>
2024-12-13 17:02:42 +0600

    initial commit

---
+++ src/main/java/project/studentis/StudentIsApplication.java
@@ -0,0 +1,13 @@
+package project.studentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/studentis/StudentIsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.studentis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
